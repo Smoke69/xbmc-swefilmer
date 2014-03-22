bccf896331fd5ba2e9b3102a30e28e825337f6f0
Magnus <magnus@gusse.se>
2014-03-22 09:42:15 +0100

    initial version

---
+++ navigation.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+from mocks import Xbmc, Xbmcplugin, Xbmcgui, Xbmcaddon
+import swefilmer
+import sys
+import urllib
+
+ACTION_NEW = 'new'
+ACTION_TOP = 'top'
+ACTION_FAVORITES = 'favorites'
+ACTION_CATEGORIES = 'categories'
+ACTION_CATEGORY = 'category'
+ACTION_SEARCH = 'search'
+ACTION_VIDEO = 'video'
+ACTION_NEXT_PAGE = 'next'
+
+class Navigation(object):
+
+    def __init__(self, xbmc, xbmcplugin, xbmcgui, xbmcaddon, swefilmer,
+                 plugin_url, handle, params):
+        self.xbmc = xbmc
+        self.xbmcplugin = xbmcplugin
+        self.xbmcgui = xbmcgui
+        self.xbmcaddon = xbmcaddon
+        self.swefilmer = swefilmer
+        self.plugin_url = plugin_url
+        self.handle = int(handle)
+        self.params = self.swefilmer.parameters_string_to_dict(params)
+        self.settings = xbmcaddon.Addon(id='plugin.video.swefilmer')
+        self.localize = self.settings.getLocalizedString
+
+    def get_credentials(self):
+        username = self.settings.getSetting('username')
+        password = self.settings.getSetting('password')
+        return (username, password)
+
+    def unikeyboard(self, default, message):
+        keyboard = self.xbmc.Keyboard(default, message)
+        keyboard.doModal()
+        if (keyboard.isConfirmed()):
+            return keyboard.getText()
+        else:
+            return None
+
+    def quality_select_dialog(self, stream_urls):
+        qualities = [s[0] for s in stream_urls]
+        dialog = self.xbmcgui.Dialog()
+        answer = 0
+        if len(qualities) > 1:
+            answer = dialog.select(self.localize(30201), qualities)
+            if answer == -1:
+                return None
+        url = stream_urls[answer][1]
+        return url
+
+    def add_menu_item(self, caption, action, total_items, url=None):
+        li = self.xbmcgui.ListItem(caption)
+        infoLabels = {'Title': caption}
+        li.setInfo(type='Video', infoLabels=infoLabels)
+        params = {'action': action}
+        if url:
+            params['url'] = url
+        item_url = self.plugin_url + '?' + urllib.urlencode(params)
+        self.xbmcplugin.addDirectoryItem(handle=self.handle, url=item_url,
+                                         listitem=li, isFolder=True,
+                                         totalItems=total_items)
+
+    def add_video_item(self, caption, url, image, action, total_items):
+        li = self.xbmcgui.ListItem(caption)
+        li.setProperty('IsPlayable', 'true')
+        if image:
+            li.setThumbnailImage(image)
+        infoLabels = {'Title': caption}
+        li.setInfo(type='Video', infoLabels=infoLabels)
+        params = {'action': action, 'url': url}
+        item_url = self.plugin_url + '?' + urllib.urlencode(params)
+        self.xbmcplugin.addDirectoryItem(handle=self.handle, url=item_url,
+                                         listitem=li, isFolder=False,
+                                         totalItems=total_items)
+
+    def start_menu(self):
+        logged_in = False
+        (username, password) = self.get_credentials()
+        if username and len(username) > 0:
+            logged_in = self.swefilmer.login(username, password)
+            if not logged_in:
+                self.xbmcgui.Dialog().ok(self.localize(30501),
+                                         self.localize(30502))
+        total_items = 5 if logged_in else 4
+        self.add_menu_item(self.localize(30101), ACTION_NEW, total_items)
+        self.add_menu_item(self.localize(30102), ACTION_TOP, total_items)
+        if logged_in:
+            self.add_menu_item(self.localize(30103), ACTION_FAVORITES,
+                               total_items)
+        self.add_menu_item(self.localize(30104), ACTION_CATEGORIES,
+                           total_items)
+        self.add_menu_item(self.localize(30105), ACTION_SEARCH, total_items)
+        return True
+
+    def new_menu(self):
+        html = self.swefilmer.new_menu_html()
+        return self.scrape_list(html)
+
+    def top_menu(self):
+        html = self.swefilmer.top_menu_html()
+        return self.scrape_list(html)
+
+    def favorites_menu(self):
+        html = self.swefilmer.favorites_menu_html()
+        return self.scrape_list(html)
+
+    def categories_menu(self):
+        html = self.swefilmer.categories_menu_html()
+        ret, pagination = self.swefilmer.scrape_categories(html)
+        total_items = len(ret)
+        for (url, name), img in ret:
+            self.add_menu_item(name, ACTION_CATEGORY, total_items, url)
+        return True
+
+    def category_menu(self):
+        if not 'browse-serier' in self.params['url']:
+            return self.next_page()
+        html = self.swefilmer.menu_html(self.params['url'])
+        ret, pagination = self.swefilmer.scrape_series(html)
+        total_items = len(ret) + len(pagination)
+        for (url, name), img in ret:
+            self.add_menu_item(name, ACTION_CATEGORY, total_items, url)
+        if pagination:
+            self.add_menu_item(self.localize(30301), ACTION_NEXT_PAGE,
+                               total_items, pagination[0])
+        return True
+
+    def search_menu(self):
+        try:
+            latest_search = self.settings.getSetting("latestSearch")
+        except KeyError:
+            latest_search = ""
+        search_string = self.unikeyboard(latest_search, "")
+        if search_string == "": return
+        self.settings.setSetting("latestSearch", search_string)
+        html = self.swefilmer.search_menu_html(search_string)
+        return self.scrape_list(html)
+
+    def next_page(self):
+        url = self.params['url']
+        html = self.swefilmer.menu_html(url)
+        return self.scrape_list(html)
+
+    def scrape_list(self, html):
+        ret, pagination = self.swefilmer.scrape_list(html)
+        total_items = len(ret) + len(pagination)
+        for (url, name), img in ret:
+            self.add_video_item(name, url, img, ACTION_VIDEO, total_items)
+        if pagination:
+            self.add_menu_item(self.localize(30301), ACTION_NEXT_PAGE,
+                               total_items, pagination[0])
+        return True
+
+    def video(self):
+        url = self.params['url']
+        html = self.swefilmer.video_html(url)
+        result = self.swefilmer.scrape_video(html)
+        if not result:
+            self.xbmcgui.Dialog().ok(self.localize(30401),
+                                     self.localize(30402),
+                                     self.localize(30403))
+            self.xbmcplugin.setResolvedUrl(
+                self.handle, succeeded=False,
+                listitem=self.xbmcgui.ListItem(''))
+            return False
+        name, description, img, streams = result
+        self.xbmc.log('video: name=' + str(name))
+        self.xbmc.log('video: description=' + str(description))
+        self.xbmc.log('video: img=' + str(img))
+        self.xbmc.log('video: streams=' + str(streams))
+        if len(url) > 1:
+            url = self.quality_select_dialog(streams)
+            if not url:
+                self.xbmcplugin.setResolvedUrl(
+                    self.handle, succeeded=False,
+                    listitem=self.xbmcgui.ListItem(''))
+                return False
+        else:
+            url = url[0][1]
+        list_item = self.xbmcgui.ListItem(name)
+        if img:
+            list_item.setThumbnailImage(img[0])
+        infoLabels = {'Title': name}
+        if description:
+            infoLabels['Plot'] = description[0]
+        list_item.setInfo(type="Video", infoLabels=infoLabels)
+        list_item.setPath(url)
+        self.xbmcplugin.setResolvedUrl(self.handle, True, list_item)
+        return True
+
+    def dispatch(self):
+        ret = False
+        if not 'action' in self.params:
+            ret = self.start_menu()
+        else:
+            action = self.params['action']
+            if action == ACTION_NEW:
+                ret = self.new_menu()
+            elif action == ACTION_TOP:
+                ret = self.top_menu()
+            elif action == ACTION_FAVORITES:
+                ret = self.favorites_menu()
+            elif action == ACTION_CATEGORIES:
+                ret = self.categories_menu()
+            elif action == ACTION_CATEGORY:
+                ret = self.category_menu()
+            elif action == ACTION_SEARCH:
+                ret = self.search_menu()
+            elif action == ACTION_VIDEO:
+                ret = self.video()
+            elif action == ACTION_NEXT_PAGE:
+                ret = self.next_page()
+        return self.xbmcplugin.endOfDirectory(self.handle, succeeded=ret,
+                                              cacheToDisc=True)
+
+# Use of standalone Navigation for testing:
+# python navigation.py <params>
+if __name__ == '__main__':
+    xbmc = Xbmc(level=Xbmc.LOGNOTICE)
+    xbmcplugin = Xbmcplugin(xbmc)
+    xbmcgui = Xbmcgui()
+    xbmcaddon = Xbmcaddon()
+    swe = swefilmer.Swefilmer(xbmc, xbmcplugin, xbmcgui, xbmcaddon)
+    navigation = Navigation(xbmc, xbmcplugin, xbmcgui, xbmcaddon, swe,
+                            'plugin', '10', '?' + sys.argv[1])
+    navigation.dispatch()
---
+++ swefilmer.py
@@ -0,0 +1,281 @@
+# -*- coding: utf-8 -*-
+import cookielib
+import HTMLParser
+import os
+import re
+import time
+import urllib
+import urllib2
+
+SAVE_FILE = True
+BASE_URL = 'http://www.swefilmer.com/'
+USERAGENT = ' Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
+
+class Swefilmer:
+
+    class MyHTTPRedirectHandler(urllib2.HTTPRedirectHandler):
+        def http_error_302(self, req, fp, code, msg, headers):
+            return urllib2.HTTPRedirectHandler.http_error_302(self, req, fp,
+                                                              code, msg,
+                                                              headers)
+
+        http_error_301 = http_error_303 = http_error_307 = http_error_302
+
+
+    def __init__(self, xbmc, xbmcplugin, xbmcgui, xbmcaddon):
+        self.xbmc = xbmc
+        self.xbmcplugin = xbmcplugin
+        self.xbmcgui = xbmcgui
+        self.xbmcaddon = xbmcaddon
+        temp = self.xbmc.translatePath(
+            os.path.join(self.xbmcaddon.Addon().getAddonInfo('profile').\
+                             decode('utf-8'), 'temp'))
+        if not os.path.exists(temp):
+            os.mkdir(temp)
+        cookiejarfile = os.path.join(temp, 'swefilmer_cookies.dat')
+        self.cookiejar = cookielib.LWPCookieJar(cookiejarfile)
+        if os.path.exists(cookiejarfile):
+            self.cookiejar.load()
+
+        cookieprocessor = urllib2.HTTPCookieProcessor(self.cookiejar)
+        opener = urllib2.build_opener(Swefilmer.MyHTTPRedirectHandler,
+                                      cookieprocessor)
+        urllib2.install_opener(opener)
+
+    def get_url(self, url, filename=None, referer=None, data=None):
+        """Send http request to url.
+        Send the request and return the html response.
+        Sends cookies, receives cookies and saves them.
+        Resonse html can be saved in file for debugging.
+        """
+        self.xbmc.log('get_url' + ((' (' + filename + ')')
+                                   if filename else '') + ': ' +
+                      str(url))
+        req = urllib2.Request(url)
+        req.add_header('User-Agent', USERAGENT)
+        if referer:
+            req.add_header('Referer', referer)
+        response = urllib2.urlopen(req, data)
+        url = response.geturl()
+        html = response.read()
+        response.close()
+        self.cookiejar.save()
+
+        if filename and SAVE_FILE:
+            filename = self.xbmc.translatePath('special://temp/' + filename)
+            file = open(filename, 'w')
+            file.write(html)
+            file.close()
+        return html
+
+    def login(self, username, password):
+        """Login to the site.
+        First check if cookies from earlier login exist and are not about
+        to expire.
+        Login has the side effect of getting cookies which are then used in
+        subsequent requests to the site.
+        """
+        # TODO: what if user changes settings for credentials and cookies
+        # are intact?
+        for cookie in self.cookiejar:
+            if cookie.name.find('phpsugar_') > -1:
+                if (cookie.expires - time.time())/3600/24 > 0:
+                    return True
+                break
+        self.cookiejar.clear()
+        url = BASE_URL + 'login.php'
+        form = {'username' : username,
+                'pass' : password,
+                'remember' : '1',
+                'ref' : '',
+                'Login' : 'Logga in' }
+        data = urllib.urlencode(form)
+        data = data.encode('utf-8') # data should be bytes
+        html = self.get_url(url, 'login.html', data=data)
+        if html.find('<div class="error_msg') > -1:
+            return False
+        return True
+
+    def convert(self, val):
+        if isinstance(val, unicode):
+            val = val.encode('utf8')
+        elif isinstance(val, str):
+            try:
+                val.decode('utf8')
+            except:
+                pass
+        return val
+
+    def parameters_string_to_dict(self, str):
+        """Parses parameter string and returns dictionary of keys and values.
+        """
+        params = {}
+        if str:
+            pairs = str[1:].split("&")
+            for pair in pairs:
+                split = pair.split('=')
+                if (len(split)) == 2:
+                    key = self.convert(urllib.unquote_plus(split[0]))
+                    value = self.convert(urllib.unquote_plus(split[1]))
+                    params[key] = value
+        return params
+
+    def unpack(self, p, a, c, k):
+        while c > 0:
+            c -= 1
+            if k[c]:
+                p = re.sub('\\b' + self.baseN(c, a) + '\\b', k[c], p)
+        return p
+
+    def baseN(self, num, b, numerals="0123456789abcdefghijklmnopqrstuvwxyz"):
+        return ((num == 0) and numerals[0]) or \
+            (self.baseN(num // b, b, numerals).lstrip(numerals[0]) + \
+                 numerals[num % b])
+
+    def addCookies2Url(self, url):
+        c = ''
+        for cookie in self.cookiejar:
+            if cookie.domain_specified and cookie.domain in url:
+                c += cookie.name + '=' + cookie.value + ';'
+        if len(c) > 0:
+            url += '|Cookie=' + urllib.quote(c)
+        return url
+
+    def parse(self, html, part_pattern, url_and_name_pattern,
+              img_pattern):
+        # check for more pages
+        pagination = [
+            BASE_URL + x for x in
+            re.findall('<div class="fastphp".*<a href="(.+?)">n&auml;sta',
+                       html)
+            if not 'class="disabled' in x]
+        html = re.findall(part_pattern, html, re.DOTALL)
+        if not html:
+            return []
+        htmlParser = HTMLParser.HTMLParser()
+        url_and_name = [(x[0], htmlParser.unescape(x[1])) for x in
+                        re.findall(url_and_name_pattern, html[0])]
+        img = [None]*len(url_and_name)
+        if img_pattern:
+            img = re.findall(img_pattern, html[0])
+            if len(img) != len(url_and_name):
+                raise Exception('found ' + str(len(img)) +
+                                ' images but ' + str(len(url_and_name)) +
+                                ' names!')
+        ret = zip(url_and_name, img)
+        return ret, pagination
+
+    def scrape_list(self, html):
+        return self.parse(
+            html,
+            part_pattern='<div class="filmcontent">(.+?)<div id="sidebar">',
+            url_and_name_pattern=\
+                '<div class="movief"><a href="(.+?)">(.+?)</a></div>',
+            img_pattern='<img src="(.+?)"[ ]+alt=.+?/></a>')
+
+    def scrape_categories(self, html):
+        return self.parse(
+            html,
+            part_pattern="<ul id='ul_categories'>(.+?)<ul class='hidden_li'>",
+            url_and_name_pattern='<li class=.+?<a href="(.+?)">(.+?)</a>',
+            img_pattern=None)
+
+    def scrape_series(self, html):
+        return self.parse(
+            html,
+            part_pattern='<ul class=\'hidden_li\'>(.+?)</ul>',
+            url_and_name_pattern='<a href="(.+?)">(.+?)</a>',
+            img_pattern=None)
+
+    def scrape_video(self, html):
+        if html.find('id="restricted_video"') > -1:
+            # registered users only, not logged in?
+            return None
+        name = re.findall('class="filmcontent".+?title="(.+?)"', html,
+                          re.DOTALL)
+        if not name:
+            name = 'unknown'
+        else:
+            name = name[0]
+        description = re.findall(
+            '>Beskrivning<.*?<p>(.+?)</p>', html, re.DOTALL)
+        self.xbmc.log('scrape_video: description=' + str(description))
+        img = re.findall(
+            '<div class="filmaltiimg">.*?<img src="(.+?)".*?</div>', html,
+            re.DOTALL)
+        self.xbmc.log('scrape_video: img=' + str(img))
+        url = re.findall('<iframe .*?src="(.+?)" ', html)
+        self.xbmc.log('scrape_video: url=' + str(url[0]))
+        if 'docs.google.com' in url[0]:
+            return name, description, img, self.scrape_googledocs(url[0])
+        document = self.get_url(url[0], 'document.html')
+        flashvars = re.findall('<param name="flashvars" value="(.+?)">',
+                               document)
+        if len(flashvars) > 0:
+            return name, description, img, self.scrape_video_vk(flashvars[0])
+        else:
+            url = self.scrape_video_registered(document)
+            if not url:
+                return
+            url = self.addCookies2Url(url)
+            return name, description, img, [('', url)]
+
+    def scrape_googledocs(self, url):
+        html = self.get_url(url, 'googledocs.html')
+        formats = re.findall('"fmt_list":"(.+?)"', html)[0].split(',')
+        streams = re.findall('"url_encoded_fmt_stream_map":"(.+?)"',
+                             html)[0].split(',')
+        urls = [self.addCookies2Url(urllib2.unquote(x).split('\\u0026')[1]
+                                    .split('\\u003d')[1]) for x in streams]
+        return zip(formats, urls)
+
+    def scrape_video_vk(self, flashvars):
+        names = [x[3:] for x in re.findall('(url[0-9]+)=.+?&amp;', flashvars)]
+        urls = re.findall('url[0-9]+=(.+?)&amp;', flashvars)
+        return zip(names, urls)
+
+    def scrape_video_registered(self, html):
+        script = re.findall(
+            '(<script type=\'text/javascript\'>eval\(function\(.*}\(.*)', html)
+        if len(script) > 0:
+            pack = re.findall(
+                '}\(\'(.+?[^\\\])\',([0-9]+),([0-9]+),\'(.+?)\'\.split',
+                script[0])[0]
+            unpacked = self.unpack(pack[0], int(pack[1]), int(pack[2]),
+                                   pack[3].split('|'))
+            url = re.findall('file:"(.+?)"', unpacked)[0]
+            self.xbmc.log('scrape_video_registered: url= ' + str(url))
+            return url
+        self.xbmc.log('scrape_video_registered: search for videoSrc')
+        videosrc = re.findall('videoSrc = "(.+?)"', html)
+        if videosrc:
+            self.xbmc.log('scrape_video_registered: url= ' + str(videosrc[0]))
+            return videosrc[0]
+
+    def new_menu_html(self):
+        url = BASE_URL + 'newvideos.html'
+        return self.get_url(url, 'new.html')
+
+    def top_menu_html(self):
+        url = BASE_URL + 'topvideos.html'
+        return self.get_url(url, 'top.html')
+
+    def favorites_menu_html(self):
+        url = BASE_URL + 'favorites.php?a=show'
+        return self.get_url(url, 'favorites.html')
+
+    def categories_menu_html(self):
+        return self.get_url(BASE_URL, 'categories.html')
+
+    def search_menu_html(self, search_string):
+        url = BASE_URL + 'search.php?' + \
+            urllib.urlencode({'keywords': search_string})
+        return self.get_url(url, 'search.html')
+
+    def menu_html(self, url):
+        return self.get_url(url, 'menu.html')
+
+    def video_html(self, url):
+        if 'ajax_request' in url:
+            url = eval(url.replace('false', 'False').replace('true', 'True'))
+        return self.get_url(url, 'video.html')
